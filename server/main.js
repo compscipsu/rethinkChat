7f12a59f87e8c421ed7f637cc55ea8b7e43df443
Jon <compscipsu@gmail.com>
2015-11-17 19:42:02 -0600

    Rooms

--- server/main.js
+++ server/main.js
@@ -16,22 +16,85 @@ var server = http.createServer(app).listen(1337);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function(socket) {
-    socket.on('message_to_server', function(data) {
-
-	r.connect({
-	    host: 'localhost',
-	    port: 28015,
-	    db: 'chat'
-	}, function(err, conn) {
-           if (err)
-              console.log(err);
-           else
-               r.table('message').insert(data).run(conn, function(err) {
-                  if (err)
-                     console.log(err);
-               });
-	});
+    socket.on('create_message', function(data) {
+        r.connect({
+            host: 'localhost',
+            port: 28015,
+            db: 'chat'
+        }, function(err, conn) {
+               if (err)
+                  console.log(err);
+               else
+                   r.table('message').insert(data).run(conn, function(err) {
+                      if (err)
+                         console.log(err);
+                   });
+        });
    });
+
+    socket.on('create_room', function(data) {
+        socket.leaveAll();
+        socket.join(data['name']);
+
+        r.connect({
+            host: 'localhost',
+            port: 28015,
+            db: 'chat'
+        }, function(err, conn) {
+            if (err)
+                console.log(err);
+            else
+                r.table('room').insert(data).run(conn, function(err) {
+                    if (err)
+                        console.log(err);
+                });
+        });
+    });
+
+    socket.on('join_room', function(data) {
+        socket.leaveAll();
+        socket.join(data['name']);
+
+        r.connect({
+            host: 'localhost',
+            port: 28015,
+            db: 'chat'
+        }, function(err, conn) {
+            r.table('message').filter({room: data['name']}).run(conn, function(err, cursor) {
+                if (err)
+                    console.log(err);
+                else
+                    cursor.each(function(err, row) {
+                        if (err)
+                            console.log(err);
+
+                        socket.emit("add_message", { message: row['message'] })
+                    });
+            });
+        });
+
+    });
+
+    r.connect({
+        host: 'localhost',
+        port: 28015,
+        db: 'chat'
+    }, function(err, conn) {
+        if (err)
+            console.log(err);
+        else
+            r.table('room').run(conn, function(err, cursor) {
+                if (err)
+                    console.log(err);
+                else
+                    cursor.each(function(err, row) {
+                        if (err)
+                            console.log(err);
+
+                        socket.emit("add_room", { message: row['name'] })
+                    });
+            });
+    });
 });
 
 r.connect({
@@ -47,7 +110,25 @@ r.connect({
             if (err)
                console.log(err);
 
-            io.sockets.emit("message_to_client", { message: row['new_val']['message'] })
+            io.sockets.to(row['new_val']['room']).emit("add_message", { message: row['new_val']['message'], author:  row['new_val']['author']})
           });
    });
+});
+
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'chat'
+}, function(err, conn) {
+    r.table('room').changes({includeInitial: false}).run(conn, function(err, cursor) {
+        if (err)
+            console.log(err);
+        else
+            cursor.each(function(err, row) {
+                if (err)
+                    console.log(err);
+
+                io.sockets.emit("add_room", { message: row['new_val']['name'] })
+            });
+    });
 });
