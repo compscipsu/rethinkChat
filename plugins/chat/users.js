5749b77116aa08f881639c3fea2901c1c91ed11a
jrausch <jrausch@morphotrust.com>
2016-02-05 08:34:14 -0600

    Changing user create to handleer

--- plugins/chat/users.js
+++ plugins/chat/users.js
@@ -6,27 +6,32 @@ var rethink = require('./rethinkdb.js');
 
 exports.createUser = function () {
   return {
-    plugins: {
-      'hapi-io': {
-        event: 'create_user',
-        post: function(ctx, next) {
-          var data = {
-            login: ctx.data['login'],
-            password: ctx.data['password']
-          };
-
-          rethink.writeToTable('user', data, function (err) {
-            if (err)
-              console.log(err);
-          });
-
-          next();
-        }
-      }
-    },
     handler: (request, reply) => {
-      //console.log("Create User");
-      reply();
+
+      var data = {
+        login: request.payload.data.login
+      };
+
+      rethink.getData('user', data, null, function (err, cursor) {
+        if (err)
+          return reply({errors: ['an error occured while trying to find existing user']});
+        else
+          cursor.each(function (err, row) {
+            if (err)
+              return reply({errors: ['an error occured while trying to find existing user']});
+
+            if (row.login === data.login) {
+              return reply({errors: ['user name already exists']});
+            }
+          });
+      });
+
+      data.password = request.payload.password;
+      rethink.writeToTable('user', data, function (err) {
+        if (err) {
+          return reply({errors: ['failed to create user']});
+        }
+      });
     }
   };
 };
